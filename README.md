0f57ab44151c416bae1be7cbc47db8e4837f4c00
NitoVal <nitoval360@gmail.com>
2024-09-08 14:33:59 -0400

    Reset

--- README.md
+++
@@ -1,3 +0,0 @@
-# StormyGame
-
-Developed with Unreal Engine 5
--- Source/StormyGame/StormyGame.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "StormyGame.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, StormyGame, "StormyGame" );
- 
