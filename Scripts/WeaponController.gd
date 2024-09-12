b318a7fc201ebca1ecb35b66ccbd30598bb74bce
SkullDragger <Lowdmik@gmail.com>
2024-09-12 19:21:50 +0100

    started work on the enviroment

--- Scripts/WeaponController.gd
+++ Scripts/WeaponController.gd
@@ -8,19 +8,19 @@ extends Node3D
 var fire_timer : float
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    fire_timer = fire_rate
-    pass # Replace with function body.
+	fire_timer = fire_rate
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-    if fire_timer < fire_rate:
-        fire_timer += delta
-        
-    if Input.is_action_pressed("fire") and fire_timer >= fire_rate:
-        fire_timer = 0
-        var bullet = bullet_prefab.instantiate()
-        root_node.add_child(bullet)
-        bullet.position = shoot_position.global_position
-        bullet.direction = -get_global_transform().basis.z
-    pass
+	if fire_timer < fire_rate:
+		fire_timer += delta
+		
+	if Input.is_action_pressed("fire") and fire_timer >= fire_rate:
+		fire_timer = 0
+		var bullet = bullet_prefab.instantiate()
+		root_node.add_child(bullet)
+		bullet.position = shoot_position.global_position
+		bullet.direction = -get_global_transform().basis.z
+	pass
--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -6,10 +6,10 @@ var health: int = 1
 
 # Called when the enemy takes damage
 func take_damage(damage: int) -> void:
-    health -= damage
-    if health <= 0:
-        die()
+	health -= damage
+	if health <= 0:
+		die()
 
 # Handle enemy death
 func die() -> void:
-    queue_free()  # Remove the enemy from the scene
+	queue_free()  # Remove the enemy from the scene
