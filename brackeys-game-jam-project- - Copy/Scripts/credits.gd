13be56c77d6ad9de99385e1d47ded052178988e2
SkullDragger <Lowdmik@gmail.com>
2024-09-15 09:18:13 +0100

    Revert "Merge remote-tracking branch 'upstream/Prod-Branch' into Prod-Branch"
    This reverts commit 6ba2337b819fcd8fc04e25d00d4acadc82939f16, reversing
    changes made to 6c2d3ac1d189abfeec4b5e0b4f7d108cf4ce460c.

--- brackeys-game-jam-project- - Copy/Scripts/credits.gd
+++
@@ -1,6 +0,0 @@
-extends Control
-
-
-func _on_credit_back_button_pressed() -> void:
-	print("Back pressed")
-	get_tree().change_scene_to_file("res://Scenes/Main Menu.tscn")
--- brackeys-game-jam-project- - Copy/Scripts/level_1.gd
+++
@@ -1,6 +0,0 @@
-extends Node3D
-
-
-func _on_in_game_back_button_pressed() -> void:
-	print("Back pressed")
-	get_tree().change_scene_to_file("res://Scenes/Main Menu.tscn")
